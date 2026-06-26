05aeea6c1592596596a5efa4ad1a142a5ca70c69
Keno38 <enovo1230@gmail.com>
2026-06-26 10:53:08 -0400

    Corregir redirectUri de MSAL para GitHub Pages

--- src/lib/msalConfig.ts
+++ src/lib/msalConfig.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId:    'c1f8a6c3-95bd-42c6-a7fd-0be2958f35ae',
     authority:   'https://login.microsoftonline.com/1b037852-4fe3-470f-bb9e-c8ba670e4653',
-    redirectUri: window.location.origin,
+    redirectUri: window.location.origin + (window.location.pathname.startsWith('/Tramites') ? '/Tramites/' : '/'),
   },
   cache: {
     cacheLocation: 'sessionStorage',
