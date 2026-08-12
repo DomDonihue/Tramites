2acc087dfbd8389a1bff80f7655b65fbb86275ed
DomDonihue <enovo@mdonihue.cl>
2026-08-12 08:42:29 -0400

    fix: permitir acceso publico al portal ciudadano de certificados

--- src/App.tsx
+++ src/App.tsx
@@ -19,27 +19,21 @@ import { ImportPage } from './pages/ImportPage'
 function ProtectedRoutes() {
   const { user, can } = useAuth()
   if (!user) return <Navigate to="/" replace />
-  return (
-    <AppLayout>
-      <Routes>
-        <Route path="/buscar" element={<BuscarPage />} />
-        <Route path="/nuevo" element={<ExpedienteFormPage />} />
-        <Route path="/expediente/:id" element={<ExpedienteFormPage />} />
-        <Route path="/estadisticas" element={<EstadisticasPage />} />
-        <Route path="/repositorio" element={<RepositorioPage />} />
-        <Route path="/desarchivo/:id" element={<DesarchivePage />} />
-        <Route path="/certificados" element={<CertificadosPage />} />
-        <Route path="/certificados/nuevo" element={<CertificadosPage nuevo />} />
-        <Route path="/certificados-web" element={<CertificadosWebPage />} />
-        <Route path="/consultar-solicitud" element={<CertificadosWebPage />} />
-        <Route path="/certificados-web/bandeja" element={<CertificadosWebBandejaPage />} />
-        {can('manageUsers') && <Route path="/setup" element={<SetupPage />} />}
-        {can('manageUsers') && <Route path="/importar" element={<ImportPage />} />}
-        {can('manageUsers') && <Route path="/usuarios" element={<UsuariosPage />} />}
-        <Route path="*" element={<Navigate to="/buscar" replace />} />
-      </Routes>
-    </AppLayout>
-  )
+  return <AppLayout><Routes>
+    <Route path="/buscar" element={<BuscarPage />} />
+    <Route path="/nuevo" element={<ExpedienteFormPage />} />
+    <Route path="/expediente/:id" element={<ExpedienteFormPage />} />
+    <Route path="/estadisticas" element={<EstadisticasPage />} />
+    <Route path="/repositorio" element={<RepositorioPage />} />
+    <Route path="/desarchivo/:id" element={<DesarchivePage />} />
+    <Route path="/certificados" element={<CertificadosPage />} />
+    <Route path="/certificados/nuevo" element={<CertificadosPage nuevo />} />
+    <Route path="/certificados-web/bandeja" element={<CertificadosWebBandejaPage />} />
+    {can('manageUsers') && <Route path="/setup" element={<SetupPage />} />}
+    {can('manageUsers') && <Route path="/importar" element={<ImportPage />} />}
+    {can('manageUsers') && <Route path="/usuarios" element={<UsuariosPage />} />}
+    <Route path="*" element={<Navigate to="/buscar" replace />} />
+  </Routes></AppLayout>
 }
 
 function AuthGate() {
@@ -47,6 +41,8 @@ function AuthGate() {
   useEffect(() => { if (user) initFromSharePoint() }, [user])
   return <Routes>
     <Route path="/" element={user ? <Navigate to="/buscar" replace /> : <LoginPage />} />
+    <Route path="/certificados-web" element={<CertificadosWebPage />} />
+    <Route path="/consultar-solicitud" element={<CertificadosWebPage />} />
     <Route path="/*" element={<ProtectedRoutes />} />
   </Routes>
 }
